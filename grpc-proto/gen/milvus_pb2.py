a7b5eb9a27707b3ffffecba8afe33dfcb179f161
bigsheeper <yihao.dai@zilliz.com>
2021-10-20 11:43:24 +0800

    update proto

--- grpc-proto/gen/milvus_pb2.py
+++ grpc-proto/gen/milvus_pb2.py
@@ -2483,7 +2483,7 @@ _SEARCHREQUEST = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-      _descriptor.FieldDescriptor(
+    _descriptor.FieldDescriptor(
       name='searchIDs', full_name='milvus.proto.milvus.SearchRequest.searchIDs', index=11,
       number=12, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
